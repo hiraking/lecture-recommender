495ea8a2b2a1fa07e05b2a7f6b70ccf97e1f4a23
hiraking <hirakitaito@gmail.com>
2023-01-02 04:15:44 +0900

    add sample useCallback, useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,24 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useCallback } from "react";
 import { Header } from "src/components/header";
 import { Main } from "src/components/main";
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <Link href="/about" onClick={handleClick}>
+        button
+      </Link>
       <Main page="index" />
     </>
   );
