41a1c2c59e4a40bb8955d5e026bcdd9415d0e70c
hiraking <hirakitaito@gmail.com>
2023-01-02 22:10:40 +0900

    add second argument to useCallback

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,18 +2,7 @@ import styles from "src/components/main/main.module.css";
 import { Center } from "src/components/center";
 import { Grid } from "src/components/grid";
 import { Headline } from "src/components/headline";
-import { useEffect } from "react";
-
 export function Main(props) {
-  useEffect(() => {
-    console.log("mount");
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      console.log("unmount");
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-
   return (
     <main className={styles.main}>
       <Headline page={props.page}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,25 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/header";
 import { Main } from "src/components/main";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((foo) => foo + 1);
-    setCount((foo) => foo + 1);
-  };
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
-  console.log(count);
+  const handleClick = useCallback(() => {
+    console.log(count);
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
+
   return (
     <>
       <Head>
