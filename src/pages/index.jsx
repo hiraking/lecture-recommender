595d4ee9ea8ee80acca688789e6c04ae2ebf61f8
hiraking <hirakitaito@gmail.com>
2023-01-03 02:03:38 +0900

    practice hook

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,9 @@ import { Main } from "src/components/main";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -14,11 +17,37 @@ export default function Home() {
   }, []);
 
   const handleClick = useCallback(() => {
-    console.log(count);
+    // console.log(count);
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
+
+  // console.log(isShow);
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("too many characters!");
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("the same string already exists!");
+        return prevArray;
+      }
+      if (!text) {
+        alert("invalid");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   return (
     <>
@@ -27,8 +56,16 @@ export default function Home() {
       </Head>
       <Header />
       <div style={{ textAlign: "center" }}>
-        <h1 style={{ color: "darkgreen" }}>{count}</h1>
+        {isShow ? <h1 style={{ color: "darkgreen" }}>{count}</h1> : null}
         <button onClick={handleClick}>button</button>
+        <button onClick={handleDisplay}>{isShow ? "hide" : "show"}</button>
+        <input type="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>add</button>
+        <ul>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
       </div>
       <Main page="index" />
     </>
