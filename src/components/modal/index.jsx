f7b33e04f387b0a09243dcddd885e5dd57dae3bb
hiraking <hirakitaito@gmail.com>
2023-01-11 17:58:39 +0900

    add react-modal

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,61 +1,41 @@
-import { useCallback, useMemo, useState } from "react";
 import Modal from "react-modal/lib/components/Modal";
-import { Search } from "src/components/modal/search";
+import { Pagenation } from "src/components/pagenation";
+import { Search } from "src/components/search";
+import { SearchResult } from "src/components/searchresults";
+import { useSearch } from "src/hooks/useSearch";
 
 export const SearchModal = (props) => {
+  const modal = props.modal;
+  const search = useSearch();
   Modal.setAppElement("body");
-  const URL = "http://127.0.0.1:8000";
-  const [searchResult, setSearchResult] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [hits, setHits] = useState(0);
-
-  const customStyles = useMemo(() => {
-    return {
-      content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-        background: "#f5dfbc",
-        width: "80%",
-        height: "80%",
-      },
-      overlay: {
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-      },
-    };
-  }, []);
-
-  const afterOpenModal = useCallback(() => {}, []);
-
-  const closeModal = useCallback(() => {
-    props.setIsOpen(false);
-  }, [props]);
 
   return (
     <Modal
-      isOpen={props.modalIsOpen}
-      onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      style={customStyles}
+      isOpen={modal.modalIsOpen}
+      onAfterOpen={modal.afterOpenModal}
+      onAfterClose={modal.afterCloseModal}
+      onRequestClose={modal.closeModal}
+      style={modal.modalStyles}
       contentLabel="Example Modal"
     >
-      <Search
-        setSearchResult={setSearchResult}
-        URL={URL}
-        setHits={setHits}
-        currentPage={currentPage}
-      />
-      {searchResult.length > 0 ? (
+      <Search search={search} />
+      {/* {search.lectures.length > 0 ? (
         <ul>
-          {searchResult.map((item) => {
-            return <li key={item["code1"]}>{item["title"]}</li>;
-          })}
+        {search.lectures.map((item) => {
+          return <li key={item["code1"]}>{item["title"]}</li>;
+        })}
         </ul>
+      ) : null} */}
+      {search.lectures.length > 0 ? (
+        <SearchResult lecture={search.lectures[0]} />
       ) : null}
-      <button onClick={closeModal}>close</button>
+      <button onClick={modal.closeModal}>close</button>
+      <Pagenation
+        page={search.page}
+        setPage={search.setPage}
+        hits={search.hits}
+        fetcher={search.fetcher}
+      />
     </Modal>
   );
 };
