d9a0366eb439871d7b10ad439b2827b8d8608a2a
hiraking <hirakitaito@gmail.com>
2023-01-19 12:19:42 +0900

    set to show error message when options are invalid

---
+++ src/components/recommendButton/index.jsx
@@ -0,0 +1,65 @@
+import { Alert, Button, Collapse } from "@mui/material";
+import { Box } from "@mui/system";
+import { useCallback, useState } from "react";
+
+const messages = [
+  "講義を登録してください",
+  "学部を選択してください",
+  "開講区分を選択してください",
+];
+
+export const RecommendButton = (props) => {
+  const { semesters, faculties, favorites, unfavorites, fetcher } = props;
+
+  const [nolecture, setNolecture] = useState(false);
+  const [nofaculty, setNofaculty] = useState(false);
+  const [nosemester, setNosemester] = useState(false);
+
+  const validateOption = useCallback(() => {
+    let flag = true;
+    if (!semesters.length) {
+      setNosemester(true);
+      flag = false;
+    }
+    if (!faculties.length) {
+      setNofaculty(true);
+      flag = false;
+    }
+    if (!favorites.length && !unfavorites.length) {
+      setNolecture(true);
+      flag = false;
+    }
+    setTimeout(() => {
+      setNosemester(false);
+      setNofaculty(false);
+      setNolecture(false);
+    }, 3000);
+    return flag;
+  }, [faculties, semesters, favorites, unfavorites]);
+
+  const executeRecommend = useCallback(() => {
+    if (validateOption()) {
+      fetcher(1);
+    }
+  }, [fetcher, validateOption]);
+
+  return (
+    <Box sx={{ height: "auto", backgroundColor: "lightgreen" }}>
+      <Button onClick={executeRecommend} variant="contained" size="large">
+        おすすめを探す
+      </Button>
+
+      <Collapse in={nolecture || nofaculty || nosemester}>
+        <Alert
+          severity="error"
+          variant="outlined"
+          sx={{ width: "max-content", textAlign: "left", margin: "0 auto" }}
+        >
+          {nolecture ? <p>・{messages[0]}</p> : null}
+          {nofaculty ? <p>・{messages[1]}</p> : null}
+          {nosemester ? <p>・{messages[2]}</p> : null}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
+};
