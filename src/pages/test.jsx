c3671c95d77d381e5adc7b8e794ac53f3b01ee79
hiraking <hirakitaito@gmail.com>
2023-01-21 10:54:03 +0900

    remove unnecessary parts from test.jsx

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,26 +1,11 @@
 import Head from "next/head";
-import { useCallback, useReducer, useState } from "react";
-import { Header } from "src/components/header";
 
 const Test = () => {
-  // action {id: {id}, lecture: {lecture}}
-  const favoritesReducer = useCallback((state, action) => {
-    if (state.includes(action.id)) {
-      setFavLectures((prev) => prev.filter((obj) => obj.id !== action.id));
-      return state.filter((i) => i !== action.id);
-    }
-    setFavLectures((prev) => [...prev, action.lecture]);
-    return [...state, action.id];
-  }, []);
-
-  const [favorites, favoritesDispatch] = useReducer(favoritesReducer, []);
-  const [favLectures, setFavLectures] = useState([]);
   return (
     <>
       <Head>
         <title>Experiment page</title>
       </Head>
-      <Header />
       <div
         style={{
           height: "950px",
